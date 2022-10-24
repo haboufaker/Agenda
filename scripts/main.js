add943866212954e9ff91c6ad97f8e23313ad0ba
Hisam Abou Faker <haboufaker@gmail.com>
2022-10-23 22:05:56 -0400

    Se agregó dinamismo con eventos y obtencion de datos del localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,128 @@
-// Declaracion de variable para nombre
-let nameInput = "";
+//Permite acceso global a todos los elementos
+window.addEventListener('load', () => {
+    //Variable global de tareas por hacer
+    toDos = JSON.parse(localStorage.getItem('toDos')) || [];
 
-// Declaracion variable numero de eventos
-let eventNumber = 0;
-let counter = 0;
+    //Constantes de nombre de usuario y formulario
+    const nameInput = document.querySelector('#name');
+    const newToDoForm = document.querySelector('#newToDoForm');
+    const username = localStorage.getItem('username') || '';
 
-// Declaracion de lista de eventos
-let toDoList = [];
+    nameInput.value = username;
 
+    //Algoritmo para cambiar el  nombre al puesto por el usuario
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
 
-// Declaracion de funciones
-class ToDo {
-    constructor(content, category, done, createdAt) {
-        this.content = content;
-        this.category = category;
-        this.done = done;
-        this.createdAt = createdAt;
-    }
-}
+    newToDoForm.addEventListener('submit', e => {
+        e.preventDefault();
 
-function populateToDoList() {
-    let todo1 = {
-        content: prompt("Ingresa la tarea a realizar"),
-        category: prompt("Trabajo o Personal?"),
-        done: false,
-        createdAt: new Date(),
+        //Objeto que represanta a las tareas
+        const toDo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
         }
-    toDoList.push(todo1);
+
+        toDos.push(toDo);
+
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+
+        // Resetea el formulario
+		e.target.reset();
+
+        displayToDos();
+    })
+
+    displayToDos();
+})
+
+//Funcion para visualizar cualquier cambio en la lista
+function displayToDos() {
+    const toDoList = document.querySelector('#toDoList');
+
+    toDoList.innerHTML = '';
+
+    //Ordena las tareas por de mas reciente a mas antigua y las agrega a la lista
+    toDos.sort(function(a, b){
+        return new Date(b.createdAt) - new Date(a.createdAt)}).forEach(toDo => {
+        const toDoItem = document.createElement('div');
+        toDoItem.classList.add('toDoItem');
+
+        const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+        input.type = 'checkbox';
+		input.checked = toDo.done;
+		span.classList.add('bubble');
+
+        if (toDo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+
+        content.classList.add('toDoContent');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${toDo.content}" readonly>`;
+		edit.innerHTML = 'Editar';
+		deleteButton.innerHTML = 'Borrar';
+
+        label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		toDoItem.appendChild(label);
+		toDoItem.appendChild(content);
+		toDoItem.appendChild(actions);
+
+		toDoList.appendChild(toDoItem);
+
+        if (toDo.done) {
+			toDoItem.classList.add('done');
+		}
+        //Evento para ver si la tarea fue finalizada o no
+		input.addEventListener('change', (e) => {
+			toDo.done = e.target.checked;
+			localStorage.setItem('toDos', JSON.stringify(toDos));
+
+			if (toDo.done) {
+				toDoItem.classList.add('done');
+			} else {
+				toDoItem.classList.remove('done');
+			}
+
+			displayToDos();
+        })
+
+        //Evento para la edicion de tareas
+        edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				toDo.content = e.target.value;
+				localStorage.setItem('toDos', JSON.stringify(toDos));
+				displayToDos();
+            })
+        })
+
+        //Evento para la eliminacion de tareas
+        deleteButton.addEventListener('click', (e) => {
+			toDos = toDos.filter(t => t != toDo);
+			localStorage.setItem('toDos', JSON.stringify(toDos));
+			displayToDos();
+        })
+    })
 }
-
-// Nombre de la persona que usara la agenda
-while (nameInput == "") {
-    nameInput = prompt("Ingrese su nombre");
-
-    if (nameInput == "") {
-        alert("Por favor no deje este campo en blanco, introduzca un nombre valido");
-    }
-    else {
-        console.log("Su nombre es" + " " + nameInput);
-    }
-}
-
-eventNumber = prompt("Introduzca el numero de eventos a agregar")
-
-
-
-// Algoritmo para agregar eventos y mostrarlos en la consola
-
-while (counter < eventNumber) {
-    populateToDoList();
-    counter++;
-}
-
-console.log(toDoList);
-
-
-// Algoritmo para marcar tareas como completadas
-toDoList.forEach(function(toDo) {
-
-    if (!toDo.done) {
-        let completion = prompt("Se completo el evento" + " " + toDo.content +"?");
-            if (completion == "si") {
-                toDo.done = true;
-            }
-    }
-});
-
-console.log(toDoList);
-
-// Algoritmo para encontrar eventos
-let eventSearch = prompt("Escriba el evento que desea buscar");
-const eventFinder = toDoList.find(toDo => toDo.content == eventSearch);
-
-console.log(eventFinder);
