7aad74ae09482bab04b11fffcbcf0504c95cef11
Hisam Abou Faker <haboufaker@gmail.com>
2022-11-05 15:08:01 -0400

    Se agregaron eventos emergentes con SweetAlert, carga de elementos de API JsonPlaceholder con fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,23 @@
-//Permite acceso global a todos los elementos
-window.addEventListener('load', () => {
-    //Variable global de tareas por hacer
-    toDos = JSON.parse(localStorage.getItem('toDos')) || [];
+//Variable global de tareas por hacer
+toDos = JSON.parse(localStorage.getItem('toDos')) || [];
 
-    //Constantes de nombre de usuario y formulario
-    const nameInput = document.querySelector('#name');
-    const newToDoForm = document.querySelector('#newToDoForm');
-    const username = localStorage.getItem('username') || '';
-
-    nameInput.value = username;
-
-    //Algoritmo para cambiar el  nombre al puesto por el usuario
-    nameInput.addEventListener('change', e => {
-        localStorage.setItem('username', e.target.value);
-    })
-
-    newToDoForm.addEventListener('submit', e => {
-        e.preventDefault();
-
-        //Objeto que represanta a las tareas
+//Funcion para cargar datos del API JSONPLACEHOLDER
+const placeHolder = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    data.forEach((element) => {
         const toDo = {
-            content: e.target.elements.content.value,
-            category: e.target.elements.category.value,
+            content: element.title,
+            category: "personal",
             done: false,
             createdAt: new Date().getTime()
         }
 
         toDos.push(toDo);
-
-        localStorage.setItem('toDos', JSON.stringify(toDos));
-
-        // Resetea el formulario
-		e.target.reset();
-
-        displayToDos();
     })
-
+    localStorage.setItem('toDos', JSON.stringify(toDos));
     displayToDos();
-})
+}
 
 //Funcion para visualizar cualquier cambio en la lista
 function displayToDos() {
@@ -105,24 +85,103 @@ function displayToDos() {
 			displayToDos();
         })
 
+        addEventListener('online', (event) => { 
+            displayToDos();
+        });
         //Evento para la edicion de tareas
         edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				toDo.content = e.target.value;
-				localStorage.setItem('toDos', JSON.stringify(toDos));
-				displayToDos();
-            })
+            swal({
+                title: 'Está segur@ de modificar el evento?',
+                icon: 'warning',
+                buttons: ["No, no quiero", "Sí, seguro"],
+                dangerMode: true,
+            }).then((result) => {
+                if (result) {
+                    const input = content.querySelector('input');
+			        input.removeAttribute('readonly');
+			        input.focus();
+			        input.addEventListener('blur', (e) => {
+				        input.setAttribute('readonly', true);
+				        toDo.content = e.target.value;
+				        localStorage.setItem('toDos', JSON.stringify(toDos));
+				        displayToDos();
+                        })
+                    }
+                })
         })
 
         //Evento para la eliminacion de tareas
         deleteButton.addEventListener('click', (e) => {
-			toDos = toDos.filter(t => t != toDo);
-			localStorage.setItem('toDos', JSON.stringify(toDos));
-			displayToDos();
+
+            swal({
+                title: 'Está segur@ de eliminar el evento?',
+                icon: 'warning',
+                buttons: ["No, no quiero", "Sí, seguro"],
+                dangerMode: true,
+            }).then((result) => {
+        
+                if (result) {
+                    swal({
+                        title: 'Borrado!',
+                        icon: 'success',
+                        text: 'El evento ha sido borrado'
+                    })
+
+                    toDos = toDos.filter(t => t != toDo);
+			        localStorage.setItem('toDos', JSON.stringify(toDos));
+			        displayToDos();
+                }
+            })
         })
     })
 }
+
+//Si la lista esta vacia carga las tareas de la API JSONPLACEHOLDER
+if (toDos.length == 0) {
+    placeHolder();
+}
+
+
+//Permite acceso global a todos los elementos
+window.addEventListener('load', () => {
+    //Constantes de nombre de usuario y formulario
+    const nameInput = document.querySelector('#name');
+    const newToDoForm = document.querySelector('#newToDoForm');
+    const username = localStorage.getItem('username') || '';
+
+    nameInput.value = username;
+
+    //Algoritmo para cambiar el  nombre al puesto por el usuario
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    })
+
+    newToDoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        //Objeto que represanta a las tareas
+        const toDo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        if (toDo.content == '') {
+            swal("Error!", "Estas tratando de agregar un evento vacio!", "error")
+        }
+        else {
+            toDos.push(toDo);
+        }
+
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+
+        // Resetea el formulario
+		e.target.reset();
+
+        displayToDos();
+    })
+
+    displayToDos();
+})
+
